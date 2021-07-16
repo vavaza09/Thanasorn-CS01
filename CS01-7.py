ed69b809de451368666c22774f432d847dc8a0c7
vavaza09 <64414782+vavaza09@users.noreply.github.com>
2021-07-16 09:36:46 +0700

    Update CS01-7.py

--- CS01-7.py
+++ CS01-7.py
@@ -1,10 +1,9 @@
 A = float(input("Number:"))
-if(A<10):
+if(A<=10):
     print('ไม่ผ่าน')
-elif(A<20):
+elif(A<=20):
     print('ปรับปรุง')
-elif(A<30):
-    print('ดีมาก') 
+elif(A<=30):
+    print('ดีมาก')
 else:
     print('error')
-    
