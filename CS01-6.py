ced7f705663d26cd0bb6aaf62a3800f797629856
vavaza09 <64414782+vavaza09@users.noreply.github.com>
2021-07-16 09:33:01 +0700

    Update CS01-7

--- CS01-6.py
+++ CS01-6.py
@@ -1,4 +1,7 @@
-A = int(input("number:"))
-if(A<10):
+A = int(input("Number A:"))
+B = float(input("Number B:"))
+C = float(input("Number C:"))
+if(A<10 | B>7 ^ C>5):
     print("Yes")
+
 
---
+++ CS01-7.py
@@ -0,0 +1,10 @@
+A = float(input("Number:"))
+if(A<10):
+    print('ไม่ผ่าน')
+elif(A<20):
+    print('ปรับปรุง')
+elif(A<30):
+    print('ดีมาก') 
+else:
+    print('error')
+    
